dc2a675d9c1ba22da0e9a59ef27305b28243106b
Priscilla Martinez <60373527+priscillaam@users.noreply.github.com>
2021-05-02 18:32:50 -0400

    Add files via upload

---
+++ src/cart.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+    <head>
+        <title>
+            Parking and Flight Bookings
+        </title>
+
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body class="">
+        <header>
+            <h1 class="logo"><a href="">Ecommerce Site Name</a></h1>
+            <h1 class="cart"><a href="">Cart</a></h1>
+            <nav>
+                <ul>
+                    <li class=""><a href="./index.html">Home</a></li>
+                    <li><a href="./flights.html">Flights</a></li>
+                    <li class=""><a href="./parking.html">Parking</a></li>
+
+                    
+                  </ul>
+                  
+            </nav>
+            
+        </header>
+
+        <div class="summary">
+            <h1> Cart </h1>
+
+        </div>
+        
+        
+
+        <script src = "script.js"></script>
+    </body>
+</html>
