433afe556ea4a2161cecf4182f9f74991718fd2d
Lucky Phan <lpt.lucky@gmail.com>
2021-05-02 20:24:17 -0400

    changed the email to username

--- src/login.php
+++ src/login.php
@@ -1,34 +1,29 @@
 <?php
-$email = $password = $err = "";
-//if(empty($_POST["email"]) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($_POST["password"])){
 	
-	 if (empty($_POST["email"])) {
-               $err.="Email empty ";
+$username = $password = $err = "";
+//if(empty($_POST["username"]) || !filter_var($username, FILTER_VALIDATE_EMAIL) || empty($_POST["password"])){
+	
+	 if (empty($_POST["username"])) {
+               $err.="Username empty ";
      }else {
-	   $email = test_input($_POST["email"]);
+	   $username = test_input($_POST["username"]);
 
-	   // check if e-mail address is well-formed
-	   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		  $err.="Invalid email format ";
-	   }
 	 }
-		 if (empty($_POST["password"])) {
+	if (empty($_POST["password"])) {
               $err.="Password empty";
     }else {
-		 		
-               $password = test_input($_POST["password"]);
+		 	 $password = test_input($_POST["password"]);
             }
 	if($err!=""){
 			 echo '<script>
 			 window.location.href = "both.php";
 			 alert("There are some errors: '.$err.'")</script>'; 
 	}
-	$user=$_POST["email"]." ".$_POST["password"];   
+	$user=$_POST["username"]." ".$_POST["password"];   
     $users=file_get_contents("users.html");
     if(str_contains($users,$user)){ 
       header("Location:index.html");
-		}else
-	{
+		}else{
 		include 'both.php';
 		$cookie = $_COOKIE[$counter]+1;
         setcookie($counter, $cookie);
